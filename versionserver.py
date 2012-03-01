6d230e658e427614309873e65f54edf11b56283a
Ondřej Vondra <ondrej.vondra@live.com>
2012-03-01 09:08:02 +0100

    fixed the totally awesome list sql

--- versionserver.py
+++ versionserver.py
@@ -10,14 +10,17 @@ application = web.application(urls, globals()).wsgifunc()
 db_file = os.path.dirname(__file__) + '/db/data.db'
 first_build_number = 1
 
-get_last_build_sql = \
-'select p.name, lc.project_id, lc.ver_a, lc.ver_b, max(lc.ver_c) maxver_c, lc.ver_build from LastBuild lc join ' \
-'(select lb.project_id, lb.ver_a, max(lb.ver_b) maxver_b from LastBuild lb join ' \
-'(select project_id, max(ver_a) maxver_a from LastBuild) as a '\
-'on lb.ver_a = a.maxver_a and lb.project_id = a.project_id) as b ' \
-'on lc.ver_a = b.ver_a and lc.ver_b = b.maxver_b and lc.project_id = b.project_id ' \
-'join Project p on p.id = lc.project_id '\
-'order by p.name desc'
+get_last_build_sql =\
+"select p.name, p.id, coalesce(v.ver_a || '.' || v.ver_b || '.' || v.maxver_c || '.' || v.ver_build, 'no build yet') from "\
+"Project p left outer join "\
+"(select lc.project_id, lc.ver_a, lc.ver_b, max(lc.ver_c) maxver_c, lc.ver_build from LastBuild lc join "\
+"(select lb.project_id, lb.ver_a, max(lb.ver_b) maxver_b from LastBuild lb join "\
+"(select project_id, max(ver_a) maxver_a from LastBuild group by project_id) as a "\
+"on lb.ver_a = a.maxver_a and lb.project_id = a.project_id group by lb.project_id) as b "\
+"on lc.ver_a = b.ver_a and lc.ver_b = b.maxver_b and lc.project_id = b.project_id group by lc.project_id "\
+") v "\
+"on p.id = v.project_id "\
+"order by p.name desc"
 
 class main:
     def GET(self, name):
@@ -93,7 +96,9 @@ class main:
         conn = sqlite3.connect(db_file)
         c = conn.cursor()
         c.execute(get_last_build_sql)
-        proj_infos = []
+        proj_info_table = '<table>'
         for r in c:
-            proj_infos[len(proj_infos):] = [r[0]+' '+str(r[2])+'.'+str(r[3])+'.'+str(r[4])]
-        return proj_infos
+            proj_info_table = proj_info_table + '<tr><td>' + r[0] + '</td><td>' + r[2] + '</td></tr>'
+
+        proj_info_table += '</table>'
+        return proj_info_table
