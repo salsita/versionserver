1320c57805eeadf129748b380b01e0888e25bcd4
ondravondra <ondrej.vondra@live.com>
2012-03-08 21:53:14 +0100

    improved more

--- versionserver.py
+++ versionserver.py
@@ -15,7 +15,7 @@ get_last_build_sql = """
 select p.name, p.id, coalesce(v.ver_a || '.' || v.ver_b || '.' || v.maxver_c || '.' || bn.ver_build, 'no build yet') ver
 from
 Project p left outer join
-(select lc.project_id, lc.ver_a, lc.ver_b, max(lc.ver_c) maxver_c, lc.ver_build from LastBuild lc join
+(select lc.project_id, lc.ver_a, lc.ver_b, max(lc.ver_c) maxver_c from LastBuild lc join
 (select lb.project_id, lb.ver_a, max(lb.ver_b) maxver_b from LastBuild lb join
 (select project_id, max(ver_a) maxver_a from LastBuild group by project_id) as a
 on lb.ver_a = a.maxver_a and lb.project_id = a.project_id group by lb.project_id) as b
