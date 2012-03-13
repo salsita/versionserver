320b2b7eca19e55337c5bfd967c279f2b74ff74c
ondravondra <ondrej.vondra@live.com>
2012-03-13 15:55:49 +0100

    Update db/initialprojs.sql

--- db/initialprojs.sql
+++ db/initialprojs.sql
@@ -2,5 +2,7 @@ insert into Project(name) values ('BargainMatchIE');
 insert into Project(name) values ('ienotifybar');
 insert into Project(name) values ('iesetuphelper');
 insert into Project(name) values ('libbhohelper');
+insert into Project(name) values ('apicawatch');
 
-insert into LastBuild(project_id, ver_a, ver_b, ver_c, ver_build) values ((select id from Project where name='BargainMatchIE'), 1, 0, 1, 1400);
+insert into LastBuild(project_id, ver_a, ver_b, ver_c, ver_build) values ((select id from Project where name='BargainMatchIE'), 1, 0, 2, 1);
+insert into LastBuild(project_id, ver_a, ver_b, ver_c, ver_build) values ((select id from Project where name='apicawatch'), 0, 10, 0, 2);
